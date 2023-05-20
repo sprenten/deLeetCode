fa355d66df25dc359eb11558f4a115c13ee393a0
dba <sprenten#gmail.com>
2023-05-20 01:18:05 -0500

    Lots of work done  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   stringTest.a
    	modified:   stringTest.c
    	modified:   stringTest.o
    
     Untracked files:
    	.divTest.c.swp
    	divTest.a
    	divTest.c
    	divTest.o
    	integers.bin
    	integers.txt
    	logTest.a
    	logTest.asm
    	logTest.c
    	logTest.o
    	notes.txt
    	pythonTest.py
    	rndIntegers.c
    	rndIntegers.o
    	stringTest.asm

--- stringaling/stringTest.c
+++ stringaling/stringTest.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+
+
+
 
 unsigned int byString(unsigned int i)
 {
@@ -13,19 +19,29 @@ unsigned int byString(unsigned int i)
 
 int main()
 {
-	unsigned int i[10] = {20,234,78.1001,5092,3,276,843,702123,512};
-	unsigned int a[10];
-	unsigned int a_count;
-	a_count=-1;
-	
-	for (int x=0;x<=9;x++)
-	{
-		int b = byString(i[x]);
+
+	FILE *fp;
+        const unsigned int NUM_READ =30000;
+	unsigned int i[NUM_READ];
+	unsigned int b,c,num;
+
+	fp=fopen("integers.bin", "rb");
+	   fread(i,sizeof(unsigned int) * NUM_READ,1,fp);
+           clock_t begin = clock();
+	   for (int y=0;y<NUM_READ; y++)
+	   {
+		c++;
+		 b = byString(i[y]);
 		if (b>>0)
 		{
+			num++;
 			printf("%d \n",b);
 		}
-	}
-
+	   }
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("%d Even Digit Numbers of %d Counted",num,c);
+	printf("Time Spent : %f\n\n",time_spent);
+	fclose(fp);
 
 }
